4e92ca43b8dc1ce3250d2a9903a39e9fec5e0f33
unknown <wesleydasilvaalmeida706@gmail.com>
2025-09-02 11:34:13 -0300

    Commit avaliação prática-Questões resolvidas

--- q2.py
+++ q2.py
@@ -1,6 +1,11 @@
+contador = 0
+maior50 = 0
+menor50 = 0
 produtos = int(input("informe a quantidade de produtos: "))
 for i in range (produtos):
-    preco = float(input("informe o preço dos produtos: "))
-if preco >= 50:
-    print(f"o numero de produtos maior que 50 é {i}")
+    preço = float(input("informe o preço dos produtos: "))
+    contador += preço
+    print(f"o valor total é {contador}")
+
+    
 
