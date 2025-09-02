0792174635c104268f813812281e19ffa702efea
unknown <wesleydasilvaalmeida706@gmail.com>
2025-09-02 11:57:54 -0300

    Commit avaliação prática_ Questões resolvidas

--- q2.py
+++ q2.py
@@ -1,6 +1,4 @@
 contador = 0
-maior50 = 0
-menor50 = 0
 produtos = int(input("informe a quantidade de produtos: "))
 for i in range (produtos):
     preço = float(input("informe o preço dos produtos: "))
